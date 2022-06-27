d4243df77c100ddc1acc28a9d15036ff060f2f86
Jownsu <jhonesdigno666@gmail.com>
2022-06-27 19:15:20 +0800

    backend initial setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,27 @@
-console.log('Server,.....')
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const PORT = process.env.PORT || 8000
+const connectDB = require('./config/db')
+
+//Connect to datavase
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to express! Whooo!' })
+})
+
+//routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => {
+  console.log('Server started on port ' + PORT)
+})
